5802c95ca8e4a69d58aac9094f1dc5a55f415342
Adriano Milanez <pf1588@fiap.com.br>
2025-05-14 09:33:23 -0300

    Alteração API

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,6 @@
 
-const API_BASE = "http://localhost:8080/frontmusics/api";
+//const API_BASE = "http://localhost:8080/frontmusics/api";
+
+const API_BASE = "https://api1tdspa-1.onrender.com";
 
 export default API_BASE;
